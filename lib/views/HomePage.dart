9e195ff099ffe38e543b70f79a4a6339074b157e
JosivanioMarinho <Josivaniom69@gmail.com>
2020-11-26 14:00:13 -0300

    Custom AppBar

--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
+import 'package:meus_contatos/widgets/CustomAppBar.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,32 +8,21 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  String _email = "";
-
-  Future _login() async {
-
-    FirebaseAuth auth = FirebaseAuth.instance;
-
-    FirebaseUser user = await auth.currentUser();
-    setState(() {
-      _email = user.email;
-    });
-  }
-
-  @override
-  void initState() {
-    _login();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
+      backgroundColor: Color(0xff457B9D),
       body: Container(
-        child: Text(_email),
+        child: SafeArea(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                CustomAppBar(),
+              ],
+            ),
+          )
+        ),
       ),
     );
   }
